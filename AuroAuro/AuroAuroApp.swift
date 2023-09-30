2de39441d75f0443b0d6355dcbef42bd7588cef6
Acasiax <yunji9039@naver.com>
2023-09-30 18:02:40 +0900

    Initial Commit

---
+++ AuroAuro/AuroAuroApp.swift
@@ -0,0 +1,20 @@
+//
+//  AuroAuroApp.swift
+//  AuroAuro
+//
+//  Created by 이윤지 on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct AuroAuroApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
